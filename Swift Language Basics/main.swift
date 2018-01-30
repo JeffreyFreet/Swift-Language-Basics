ce7c369001aa82d2b72943f12671526daecb7eb9
Jeffrey Freet <jpfvx8@Jeffreys-MacBook-Pro.local>
2018-01-30 13:01:01 -0600

    First Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Jeffrey Freet on 1/30/18.
+//  Copyright © 2018 Jeffrey Freet. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
